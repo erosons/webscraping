924eb3b895b82500c9fbb26a946e8359d79c6846
erosons <eromonsei.o.samson@gmail.com>
2021-07-29 00:58:58 -0500

    code update

---
+++ downloading_files_from_web.py
@@ -0,0 +1,24 @@
+import requests
+import sys
+import csv
+
+res = requests.get('https://filesamples.com/samples/document/txt/sample1.txt')
+if res.status_code == 200 or requests.codes.ok == True:
+    print(len(res.text))
+    # writing the file to a csv file.
+    with open('test.csv', 'w') as file:
+        writer = csv.writer(file)
+        writer.writerow(res.text)
+
+        file.close()
+else:
+    raise Exception("Failed to established connection")
+
+
+# Alternatively
+
+res = requests.get('https://inventwithpython.com/page_that_does_not_exist')
+try:
+    print(res.raise_for_status())
+except Exception as exec:
+    print('Check you connections %s' % (exec))
